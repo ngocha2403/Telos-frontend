c2fca49b5e764ef2e0ce5bbb2e10f7fbc8aae984
ngocha2403 <habuithingocc@gmail.com>
2025-04-07 20:14:30 +0700

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,24 @@ module.exports = {
       colors: {
         primary: "#FF12D0",
         primary2: "#8C30F5",
-        primary3: "#D6B1FF"
+        primary3: "#D6B1FF",
+        primary4: "#18191F",
+        transparentBlack: "00000050",
+        brandcolor: "#D5FAFC",
+        labelcolor: "#2EC5CE",
+        textcolor: "#18191F",
       },
       spacing: {
         18: "72px",
+        7.5: "30px",
+      },
+      fontFamily: {
+        'nanum-pen': ['"Nanum Pen Script"', 'cursive'],
       },
       fontSize: {
         h1: ["72px", "98px"],
+        h2: ["48px", "64px"],
+        h5: ["24px", "32px"],
         lead1: ["18px","32px"]
       }
     }
